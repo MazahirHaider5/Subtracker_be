3abf22d766e986ed57dfa4cdc171be2e9492333e
CodeWithHaiderr <mazahirhaider98@gmail.com>
2025-01-13 12:20:42 +0500

    fixes

--- src/controllers/appleAuth.controller.ts
+++ src/controllers/appleAuth.controller.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
-import passport, { session } from "passport";
+import passport from "passport";
 import AppleStrategy from "passport-apple";
 import User from "../models/users.model";
-import { generateAccessToken, generateRefreshToken } from "../utils/jwt";
+import fs from "fs";
 
 passport.use(
   new AppleStrategy(
@@ -23,11 +23,6 @@ passport.use(
       profile: { email?: string; name?: string },
       done: (error: any, user?: any) => void
     ) => {
-      console.log("Apple Strategy Triggered");
-      console.log("Access Token:", accessToken);
-      console.log("Refresh Token:", refreshToken);
-      console.log("ID Token:", idToken);
-      console.log("Profile:", profile);
       try {
         const email = profile.email || idToken?.payload?.email;
         const name = profile.name || "Apple user";
@@ -39,15 +34,12 @@ passport.use(
           user = new User({
             email,
             name,
-            is_verified: true,
-            appleId : idToken?.sub,
+            is_verified: true
           });
           await user.save();
         }
-        const newAccessToken = generateAccessToken(user);
-        const newRefreshToken = generateRefreshToken(user);
 
-        done(null, { user, accessToken: newAccessToken, refreshToken: newRefreshToken });
+        done(null, user);
       } catch (error) {
         done(error, false);
       }
@@ -61,46 +53,10 @@ export const loginWithApple = passport.authenticate("apple", {
 });
 
 // Apple callback route
-export const appleCallback = (req: Request, res: Response, next: any) => {
-  console.log("Apple Callback triggered");
-
-  passport.authenticate("apple", { session: true }, (err: Error | null, user: any, info: any) => {
-    console.log("session in appleCallback :", session);
-    
-    if (err || !user) {
-      console.log("Error or no user: ", err, info);
-      return res.status(401).json({
-        success: false,
-        message: "Authentication failed",
-      });
-    }
-
-    const { accessToken, refreshToken } = user;
-    console.log("User authenticated, tokens generated:");
-    console.log("Access Token:", accessToken);
-    console.log("Refresh Token:", refreshToken);
-
-    console.log("User authenticated, tokens generated:", accessToken, refreshToken);
-
-    res.cookie("accessToken", accessToken, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: 24 * 60 * 60 * 1000,
-    });
-
-    res.cookie("refreshToken", refreshToken, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
-
-    return res.status(200).json({
-      success: true,
-      message: "Login successful",
-      user,
-    });
-  })(req, res, next);
+export const appleCallback = (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "Login Successful",
+    user: req.user
+  });
 };
-
