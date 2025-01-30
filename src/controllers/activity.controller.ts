204bbce6873f86396aa7cba28032d06be7d327dc
Naveed123-ux <naveedzafar123abx@gmail.com>
2025-01-30 18:04:18 +0500

    updated

---
+++ src/controllers/activity.controller.ts
@@ -0,0 +1,19 @@
+import Activity from "../models/activity.model";
+import { Request, Response } from "express";
+
+export const getAllActivities = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const activities = await Activity.find().populate({
+      path: "userId",
+      select: "email"
+    });
+    res.status(200).json({ success: true, activities });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Error fetching activities" });
+  }
+};
